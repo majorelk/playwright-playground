d874e0ef4b35de308456be0b4a47f30a0484edcb
Daniel <daniel@weareaduro.com>
2022-05-14 15:43:35 +0100

    notes for parallel testing, extracted input to constant

--- src/test/java/authorization/whenAuthorizingForAccess.java
+++ src/test/java/authorization/whenAuthorizingForAccess.java
@@ -7,7 +7,11 @@ import com.microsoft.playwright.*;
 public class whenAuthorizingForAccess {
   private static final String USER_NAME = null;
   private static final String PASS_WORD = null;
+  public static final String USERNAME_FIELD = "input[name='email']";
+  public static final String PASSWORD_FIELD = "input[name='password']";
+  
   // static LoginPage login;
+  
   static Playwright playwright = Playwright.create();
   static Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
       .setHeadless(false));
@@ -17,8 +21,8 @@ public class whenAuthorizingForAccess {
   public static void loginToApplicationWith() {
     page.navigate("https://www.amazon.com/");
     // login.withUi(USER_NAME, PASS_WORD);
-    page.fill("input[name='email']", USER_NAME);
-    page.fill("input[name='password']", PASS_WORD);
+    page.fill(USERNAME_FIELD, USER_NAME);
+    page.fill(PASSWORD_FIELD, PASS_WORD);
     page.waitForNavigation(() -> {
       page.click("input[type='submit']");
     });
